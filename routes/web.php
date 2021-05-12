93e11f18aa73d4c6022929aa18506467aa1c2a43
jonatanhp <jonatan.hp@upeu.edu.pe>
2021-05-12 13:02:12 -0500

    second commit

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,11 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('/Onpe/users', OnpeUsersController::class);
+
+//Route::resource(['/Onpe/users' => OnpeUsersController::class ]);
+
+//Route::resource(['/Onpe/users' => OnpeUsersController::class], ['as' => 'Onpe']);
+
+//Route::resource('/Onpe/users', OnpeUsersController::class , ['as' => 'Onpe'] );
